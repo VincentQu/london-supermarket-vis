75519c42bd76914ea237bc206f2cb3717f0bb79a
VincentQu <focusitall@me.com>
2014-11-21 16:45:49 +0000

    Cleaning up #3 (commit directly from RStudio)

--- R/2_main_visualisation.R
+++ R/2_main_visualisation.R
@@ -9,14 +9,19 @@ library(grid)
 
 load("supermarket_data.rda")
 
-# Plotting location of all London supermarkets
+# Plotting location of all London supermarkets (not very insightful)
 ggplot() + 
+  # Boroughs
   geom_polygon(data = boroughs, aes(x = long, y = lat, group = group), 
                fill = "#333333", color = "#444444") +
+  # Thames
   geom_polygon(data = thames, aes(x = long, y = lat),
                fill = "#77A2B8", color = NA) +
+  # Supermarkets
   geom_point(data = supermarkets, aes(x = Lon, y = Lat, color = Retailer)) +
+  # Coordinate system
   coord_map() + 
+  # Theme
   theme(
     panel.background = element_rect(fill="#444444"),
     panel.grid = element_blank(),
@@ -34,16 +39,21 @@ otherSupermarkets <- filter(supermarkets, !(Retailer %in% c("Tesco", "Sainsburys
 
 # Map top supermarkets -- all on one map
 ggplot() + 
+  # Boroughs
   geom_polygon(data = boroughs, aes(x = long, y = lat, group = group), 
                fill = "#F9F5F1", color = "#E9E5E1", size = 0.8) +
+  # Thames
   geom_polygon(data = thames, aes(x = long, y = lat),
                fill = "#77A2B8", color = NA) +
+  # Supermarkets
   geom_point(data = topSupermarkets, aes(x = Lon, y = Lat, color = Retailer, shape = Retailer),
              size = 4) +
-  coord_map() + 
   scale_color_manual(values = c("#095530", "#E87907", "#2E4496", "#6AA628")) +
-  scale_shape_manual(values = c("M", "S", "T", "W")) +
-  guides(shape = guide_legend(override.aes = list(size=6))) +
+  scale_shape_manual(values = c("M", "S", "T", "W"),
+                     guide = guide_legend(override.aes = list(size=6))) +
+  # Coordinate system
+  coord_map() + 
+  # Theme
   theme(
     panel.background = element_rect(fill="#E9E5E1"),
     panel.grid = element_blank(),
@@ -59,7 +69,7 @@ ggplot() +
   )
 
 # Map top supermarkets -- each one into on an individual map
-# Change order ov levels for visualisation purposes
+# Change order of levels for visualisation purposes
 topSupermarkets  <- mutate(topSupermarkets,
                            Retailer = factor(Retailer, levels =
                                                c("Sainsburys", "Tesco", "Waitrose", "Marks And Spencer")))
@@ -68,17 +78,19 @@ ggplot() +
   # Boroughs
   geom_polygon(data = boroughs, aes(x = long, y = lat, group = group), 
                fill = "#F9F5F1", color = "#E9E5E1", size = 0.8) +
-  # River Thames
+  # Thames
   geom_polygon(data = thames, aes(x = long, y = lat),
                fill = "#77A2B8", color = NA) +
   # Supermarkets
   geom_point(data = topSupermarkets, aes(x = Lon, y = Lat, color = Retailer, shape = Retailer),
              size = 4.5) +
-  # Facet by Retailer
-  facet_wrap(~ Retailer) +
-  coord_map() + 
   scale_color_manual(values = c("#E87907", "#2E4496", "#6AA628", "#095530")) +
   scale_shape_manual(values = c("S", "T", "W", "M")) +
+  # Facet by Retailer
+  facet_wrap(~ Retailer) +
+  # Coordinate system
+  coord_map() + 
+  # Theme 
   theme(
     axis.ticks = element_blank(),
     axis.title = element_blank(),
@@ -96,24 +108,29 @@ ggplot() +
 TSRatio <- supermarkets %>%
   filter(Retailer %in% c("Tesco", "Sainsburys")) %>%
   group_by(Borough, Retailer) %>%
-  tally() %>%
-  summarise(TSRatio = n[2]/n[1]) %>%
+  tally() %>%  # refers to the Retailer groups
+  summarise(TSRatio = n[2]/n[1]) %>%  # refers to the Borough groups
   arrange(desc(TSRatio))
 
 boroughs <- merge(boroughs, TSRatio, by.x = "BOROUGH", by.y = "Borough")
 
 ggplot(boroughs, aes(x = long, y = lat, group = group, fill = TSRatio)) +
+  # Boroughs
   geom_polygon(size = 0.7, colour = "#E9E5E1") + 
   scale_fill_gradient2(low = "#FA6900", mid = "white", high = "#00B5D1", midpoint = 1,
-                       guide = guide_colorbar(barwidth = 2, barheight = 10, nbin = 100)) +
+                       guide = guide_colorbar(barwidth = 2, barheight = 10, nbin = 50)) +
+  # Coordinate system
   coord_map() +
+  # Map title
   ggtitle(label = "Tesco/Sainsbury's Ratio") +
+  # Annotations
   annotate("text", x = 0.08, y = 51.69, hjust = 0, color = "#696561", label = "The darker the shade of blue") +
   annotate("text", x = 0.08, y = 51.67, hjust = 0, color = "#696561", label = "the higher the relative frequency") +
   annotate("text", x = 0.08, y = 51.65, hjust = 0, color = "#696561", label = "of Tesco branches") +
   annotate("text", x = -0.55, y = 51.33, hjust = 0, color = "#696561", label = "Only three boroughs") +
   annotate("text", x = -0.55, y = 51.31, hjust = 0, color = "#696561", label = "(Sutton, Camden, and Wandsworth)") +
   annotate("text", x = -0.55, y = 51.29, hjust = 0, color = "#696561", label = "have more Sainsbury's than Tesco branches") +
+  # Theme
   theme(
     text = element_text(family = "Oswald"),
     panel.background = element_rect(fill="#E9E5E1"),
